ddf45a02b0c873761308a0f778f3a4aa16357f2f
adamwilhelmi <zaq1xsw2>
2014-04-22 11:39:18 -0400

    Adam - added some code to the client for creating the GUI

--- GroupThinkClient/src/groupthinkclient/GroupThinkClient.java
+++ GroupThinkClient/src/groupthinkclient/GroupThinkClient.java
@@ -1,6 +1,11 @@
 package groupthinkclient;
 
 import GroupThink.GTP.*;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -15,8 +20,17 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import javax.swing.*;
 
-public class GroupThinkClient {
+import static javax.swing.JOptionPane.*;
+
+@SuppressWarnings("MagicConstant")
+public class GroupThinkClient extends JFrame {
+    //Constants
+    private final int GUI_WIDTH = 500;
+    private final int GUI_HEIGHT = 300;
+
+    //Communication/networking variables
     static int PORT = 2606;
     static DataOutputStream dos = null;
     static DataInputStream dis = null;
@@ -31,20 +45,59 @@ public class GroupThinkClient {
     static InetAddress inet;
     static int myID;
 
+    //GUI-related variables
+    String username;
+
 
     public static void main(String[] args) {
         
         UDPClient.initialize(PORT, hostname);
-        
-        System.out.println("Enter a requested username.");
-        String input="";
-        Scanner scan = new Scanner(System.in);
-        input = scan.nextLine();
-        
-        while(!input.equals("exit") && !requestUsername(input))
-            input=scan.nextLine();
-        
-        System.out.println("Goodbye.");
+
+        //commenting out following code to implement similar functionality in GUI
+//        System.out.println("Enter a requested username.");
+//        String input="";
+//        Scanner scan = new Scanner(System.in);
+//        input = scan.nextLine();
+//
+//        while(!input.equals("exit") && !requestUsername(input))
+//            input=scan.nextLine();
+//
+//        System.out.println("Goodbye.");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GroupThinkClient client = new GroupThinkClient();
+                client.setVisible(true);
+            }
+        });
+
+    }
+
+    public GroupThinkClient(){
+        RTextScrollPane sp;
+        JPanel cp = new JPanel(new BorderLayout());
+
+        RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+
+        sp = new RTextScrollPane(textArea);
+        cp.add(sp);
+
+        setContentPane(cp);
+
+        setTitle("GroupThink Client");
+        setSize(GUI_WIDTH, GUI_HEIGHT);
+        setLocationRelativeTo(null); //<-- centers the gui on screen
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pack();
+        username = showInputDialog(cp, "Please enter your requested username:");
+//        if(!requestUsername(username)){
+//            do{
+//                username = showInputDialog(cp, "Username not accepted. Please try a different name:");
+//            } while (!requestUsername(username));
+//        }
     }
     
     
