439615fc9d58d053c96c675df94ac34ccdd91f04
adamwilhelmi <zaq1xsw2>
2014-05-12 11:05:59 -0400

    Adam: Changed a multi-catch to generic exception

--- GroupThinkClient/src/groupthinkclient/GroupThinkClient.java
+++ GroupThinkClient/src/groupthinkclient/GroupThinkClient.java
@@ -108,11 +108,7 @@ public class GroupThinkClient extends JFrame {
                             break;
                         }
                     }
-                } catch (ClassNotFoundException | 
-                         IllegalAccessException | 
-                         InstantiationException | 
-                         UnsupportedLookAndFeelException e
-                        ) {
+                } catch (Exception e) {
                     // If Nimbus is not available, you can set the GUI to another look and feel.
                     e.printStackTrace();
                 }
